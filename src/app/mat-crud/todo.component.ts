229b920aa9ea0cbe6ca26243044b0c54a4e9edb2
w4web <w4websolution@gmail.com>
2022-10-19 20:00:13 +0530

    Rxjs

--- src/app/mat-crud/todo.component.ts
+++ src/app/mat-crud/todo.component.ts
@@ -1,26 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
+import { Todo } from '../shared/models/todo.model';
 import { TodoEditComponent } from './todo-edit/todo-edit.component';
-
-export interface PeriodicElement {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
-}
-
-const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
-];
+import { TodoService } from './todo.service';
 
 @Component({
   selector: 'app-todo',
@@ -31,16 +13,25 @@ const ELEMENT_DATA: PeriodicElement[] = [
 export class TodoComponent implements OnInit {
 
   displayedColumns: string[] = ['position', 'name', 'weight', 'symbol', 'actions'];
-  dataSource = ELEMENT_DATA;
+  dataSource!: Todo[];
 
   theme = '';
 
-  constructor(public dialog: MatDialog) { }
+  constructor(public dialog: MatDialog, public todoService: TodoService) { }
 
   ngOnInit(): void {
+    this.todoService.allTodo().subscribe({
+      next: (res: any) => {
+        this.dataSource = res;
+        console.log("Responce..", res);
+      },
+      error: (err: any) => {
+        console.log(err);
+      }
+    });
   }
 
-  editDialog(element?: PeriodicElement): void {
+  editDialog(element?: Todo): void {
     const dialogRef = this.dialog.open(TodoEditComponent, {
       width: '250px',
       data: element,
